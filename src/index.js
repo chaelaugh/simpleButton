10574b3d22ec881b70cc494daeb52da57317ccf5
人生海海 <quinlandg@outlook.com>
2018-01-03 14:18:34 +0800

    test

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class SimpleBtn extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <button>1111</button>
+            </div>
+        );
+    }
+}
